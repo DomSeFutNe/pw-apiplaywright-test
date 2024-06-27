728fce554c22ba3285a7d797ecd97617cca6787e
Dominic Seel <dominic.seel@futurenext.de>
2024-06-27 10:16:11 +0200

    init playwright

---
+++ tests/workingWithApi.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://conduit.bondaracademy.com/');
+})
+
+test('has title', async ({ page }) => {
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
